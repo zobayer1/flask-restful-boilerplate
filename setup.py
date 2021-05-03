3d73d9dc5d5d593b845ad40b9690a507282b8b14
Zobayer Hasan <zobayer1@gmail.com>
2021-05-04 03:35:35 +0600

    Add project setup files

---
+++ setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from os import path
+from setuptools import find_packages, setup
+
+setup_dependencies = [
+    "wheel",
+    "setuptools-scm",
+]
+
+install_dependencies = [
+    "importlib-metadata;python_version<'3.8'",
+]
+
+try:
+    docs_file = path.join(path.abspath(path.dirname(__file__)), "README.md")
+    with open(docs_file, encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = __doc__
+
+setup(
+    name="flask-tutorial",
+    url="https://github.com/zobayer1/flask-tutorial",
+    license="MIT",
+    author="Zobayer Hasan",
+    author_email="zobayer1@gmail.com",
+    description="RESTful application server development with python flask",
+    keywords="python flask restful api server development",
+    long_description=long_description,
+    use_scm_version=True,
+    setup_requires=setup_dependencies,
+    packages=find_packages(exclude=["docs", "tests"]),
+    include_package_data=True,
+    zip_safe=True,
+    platforms=["posix"],
+    install_requires=install_dependencies,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
---
+++ tests/test_contexts.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
+def test_application_loads():
+    """Test fails if tox cannot run this"""
+    assert True
