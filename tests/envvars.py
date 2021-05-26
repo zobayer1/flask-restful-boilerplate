fcb14aca428ca55bd6a2e1247f1f27f567f65120
Zobayer Hasan <zobayer1@gmail.com>
2021-05-27 00:24:01 +0600

    Set test environment LOGGING_ROOT to current working directory

--- tests/envvars.py
+++ tests/envvars.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 variables = {
-    "LOGGING_ROOT": "/tmp",
+    "LOGGING_ROOT": ".",
     "FLASK_SECRET": "bb9ba2817ef62e261d3adaf90c2727bb",
     "FLASK_ENV": "testing",
 }
