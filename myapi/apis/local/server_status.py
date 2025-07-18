3b0c1d5e61343619505d100b4b2130cf2e0a18ef
Zobayer Hasan <zobayer1@gmail.com>
2025-07-18 20:47:36 +0600

    Update API description

--- myapi/apis/local/server_status.py
+++ myapi/apis/local/server_status.py
@@ -6,7 +6,7 @@ from myapi.commons.utils.app_utils import app_version
 
 
 class ServerStatus(MethodResource):
-    @doc(tags=["local"], description="Get server health status")
+    @doc(tags=["local"], description="Get server running status")
     def get(self):
         app.logger.info("Health status request received")
         return {"server": f"{app.name} v{app_version(app.name)}", "status": "running"}, 200
